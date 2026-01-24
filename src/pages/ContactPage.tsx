a2dc301f709748d4ffa560c09e72096635e1fc6e
Subhash6102003 <167321173+Subhash6102003@users.noreply.github.com>
2026-01-24 15:46:03 +0530

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -73,14 +73,6 @@ const ContactPage = () => (
     <CTASection />
   </main>
 );
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17895816129"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'AW-17895816129');
-</script>
 
 export default ContactPage;
